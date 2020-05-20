8006adad0e3d1d86604934b5eaaeab096bbbfdbd
ayfzfs <2986613902@qq.com>
2020-05-20 22:22:23 +0800

    submit

---
+++ WordCount/源.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+	FILE *op;
+	int fn = 0, wn = 1;
+	char c;
+	printf("D:\\软件工程\\WordCount\\Debug\\test.txt\n");
+	op = fopen("D:\\软件工程\\WordCount\\Debug\\test.txt", "r");
+	while (!feof(op))
+	{
+		c = fgetc(op);
+		if (c == ' ' || (c >= 'a'&&c < 'z') || (c >= 'A'&&c < 'Z') || c == '\t' || c == '\n')
+			fn++;
+		if (c == ' ' || c == '，')
+			wn++;
+	}
+	printf("字符数：%d\n", fn);
+	printf("单词数：%d", wn);
+	return 0;
+}
